f087deb2f9d6dcce81e7bc65b3aa1c309dcb8eab
Johan <Johanlinck96@gmail.com>
2024-06-21 10:46:11 +0200

    init

---
+++ DeletionGameListener.py
@@ -0,0 +1,24 @@
+import os
+import win32api 
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+_listenign = True
+
+
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
+
+observer = Observer()
+event_handler = LoggingEventHandler()
+
+observer.schedule(event_handler, desktop, recursive=False )
+
+
+observer.start()
+
+while (observer.is_alive)
+    try 
+
+event_handler.on_deleted += function () => {
+
+}
