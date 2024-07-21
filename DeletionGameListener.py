b823409862f72131be714b24ac9b99175ccba5dc
ToonLinck <Johanlinck96@gmail.com>
2024-07-21 15:54:03 +0200

    watchdog basic funktionalität

--- DeletionGameListener.py
+++ DeletionGameListener.py
@@ -1,23 +1,41 @@
+import time
 import os
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
-
-_listenign = True
-
-
-desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
-
-observer = Observer()
-event_handler = LoggingEventHandler()
-
-observer.schedule(event_handler, desktop, recursive=False )
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+import logging
 
 
-observer.start()
+class DeletionHandler(FileSystemEventHandler):
+     def on_deleted(self, event: FileSystemEvent):
+          print(event.src_path)
+          print("file deleted")
+          #Gelöschte Datei erkannt, Breakout wird gestartet
 
-while (observer.is_alive)
-    try 
 
-event_handler.on_deleted += function () => {
+if __name__ == "__main__":
+    print("Breakout Listener Startup")
 
-}
+    # Der oberste Dateipfad, bei dem der Watchdog die observation beginnt
+    ground_path = os.path.abspath(os.sep)
+    
+    # ein Eventhandler wird erstellt 
+    event_handler = DeletionHandler()
+    observer = Observer()
+    observer.schedule(event_handler, ground_path, recursive=True )
+    observer.start()
+
+    try: 
+         while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
+        print("stopped all processes of breakout listener")
+
+
+
+
+#desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
+
+
+
